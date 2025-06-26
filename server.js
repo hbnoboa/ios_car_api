df2135dee9fef5126d2966371486ecfcad6ed3b9
hbnoboa <hbnoboa11@gmail.com>
2025-06-26 17:20:22 -0300

    main

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ app.use("/api/measures", auth, auditLog, measureRoute);
 app.use("/api/quadrants", auth, auditLog, quadrantRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
 }
 
